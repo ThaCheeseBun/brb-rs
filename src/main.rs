b987f16306d98efda386688bd9893dd4ad00af56
ThaCheeseBun <36112086+ThaCheeseBun@users.noreply.github.com>
2023-05-02 17:14:53 +0200

    add day count

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn parse_time(mut s: u128) -> String {
 
     // huge math
     let mut m = s / 60;
-    let h = m / 60;
-    m = m - ( h * 60 );
-    s = s - ( h * 60 * 60 ) - ( m * 60);
+    let mut h = m / 60;
+    let d = h / 24;
+    h = h - ( d * 24 );
+    m = m - ( d * 24 * 60 ) - ( h * 60 );
+    s = s - ( d * 24 * 60 * 60 ) - ( h * 60 * 60 ) - ( m * 60);
 
     // singular or plural
+    let d_str = if d == 1 {
+        "day"
+    } else {
+        "days"
+    };
     let h_str = if h == 1 {
         "hour"
     } else {
@@ -28,7 +35,9 @@ fn parse_time(mut s: u128) -> String {
     };
 
     // finally, format string
-    if h > 0 {
+    if d > 0 {
+        format!("{} {}, {} {}, {} {}, {} {}", d, d_str, h, h_str, m, m_str, s, s_str)
+    } else if h > 0 {
         format!("{} {}, {} {}, {} {}", h, h_str, m, m_str, s, s_str)
     } else if m > 0 {
         format!("{} {}, {} {}", m, m_str, s, s_str)
